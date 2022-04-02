255a070f8b0e1b2b5ef4fb77a56a5f2ffe4e4c36
Valentin LEGEAY <valentin.legeay@gmail.com>
2022-04-03 01:04:47 +0200

    Add Wave & Mob Spawning system

--- Source/MedievalTD/MedievalTDGameModeBase.cpp
+++ Source/MedievalTD/MedievalTDGameModeBase.cpp
@@ -3,3 +3,128 @@
 
 #include "MedievalTDGameModeBase.h"
 
+#include "GameFramework/GameSession.h"
+
+AMedievalTDGameModeBase::AMedievalTDGameModeBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMedievalTDGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	AActor* playerStart = FindPlayerStart(PlayerController);
+
+	if(playerStart)
+	{
+		CrystalPosition = playerStart->GetActorLocation();
+	}
+	else if(PlayerController)
+	{
+		CrystalPosition = PlayerController->GetSpawnLocation();
+	}
+
+	if(CrystalPedestalClass)
+	{
+		GetWorld()->SpawnActor(CrystalPedestalClass, &CrystalPosition);
+	}
+
+	TimeBeforeNextWave = TimeBetweenWaves;
+}
+
+void AMedievalTDGameModeBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if(IsWaveOngoing)
+	{
+		TickWave(DeltaSeconds);
+	}
+	else
+	{
+		TickBetweenWave(DeltaSeconds);
+	}
+}
+
+void AMedievalTDGameModeBase::TickWave(float DeltaSeconds)
+{
+	if(WaveSpawnedMonsters < WaveTotalMonsters)
+	{
+		SpawnMobTimer += DeltaSeconds;
+		while(SpawnMobTimer >= SpawnMobInterval)
+		{
+			SpawnRandomMob();
+			SpawnMobTimer -= SpawnMobInterval;
+		}
+	}
+}
+
+void AMedievalTDGameModeBase::TickBetweenWave(float DeltaSeconds)
+{
+	TimeBeforeNextWave -= DeltaSeconds;
+
+	if(TimeBeforeNextWave <= 0.0f)
+	{
+		StartNextWave();
+	}
+}
+
+void AMedievalTDGameModeBase::StartNextWave()
+{
+	IsWaveOngoing = true;
+
+	CurrentWaveDefinition = &WaveDefinitions[FMath::Min(WaveIndex,WaveDefinitions.Num() - 1)];
+	WaveRemainingMonsters = 0;
+
+	RemainingMobToSpawn.Empty();
+
+	for (auto MobCount : CurrentWaveDefinition->MobCount)
+	{
+		WaveRemainingMonsters += MobCount.Value;
+		RemainingMobToSpawn.Add(MobCount.Key, MobCount.Value);
+	}
+
+	SpawnMobInterval = CurrentWaveDefinition->TotalSpawnTime / WaveRemainingMonsters;
+	SpawnMobTimer = 0.0f;
+	WaveSpawnedMonsters = 0;
+	WaveTotalMonsters = WaveRemainingMonsters;
+}
+
+void AMedievalTDGameModeBase::SpawnRandomMob()
+{
+	const int32 count = RemainingMobToSpawn.Num();
+
+	const int32 indexToPickFrom = FMath::RandRange(0, count);
+
+	TSubclassOf<AMonsterAI> pickedMob = nullptr;
+	int i = 0;
+	for (const auto MobToSpawn : RemainingMobToSpawn)
+	{
+		if(i == indexToPickFrom)
+		{
+			pickedMob = MobToSpawn.Key;
+		}
+	}
+
+	if(pickedMob)
+	{
+		int32 remainingMob = RemainingMobToSpawn[pickedMob];
+		remainingMob--;
+
+		if(remainingMob > 0)
+		{
+			RemainingMobToSpawn[pickedMob] = remainingMob;
+		}
+		else
+		{
+			RemainingMobToSpawn.Remove(pickedMob);
+		}
+
+		const FVector spawnLocation = CrystalPosition - FVector(1000, FMath::RandRange(-300, 300), 0);
+		const FRotator rot = (CrystalPosition - spawnLocation).GetSafeNormal().Rotation();
+		AMonsterAI* MonsterAI = Cast<AMonsterAI>(GetWorld()->SpawnActor(pickedMob, &spawnLocation, &rot));
+		MonsterAI->TargetLocation = CrystalPosition;
+	}
+}
--- Source/MedievalTD/MedievalTDGameModeBase.h
+++ Source/MedievalTD/MedievalTDGameModeBase.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "FWaveDefinition.h"
 #include "GameFramework/GameModeBase.h"
 #include "MedievalTDGameModeBase.generated.h"
 
@@ -13,5 +14,57 @@ UCLASS()
 class MEDIEVALTD_API AMedievalTDGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	AMedievalTDGameModeBase();
 	
+	virtual void BeginPlay() override;
+	
+	void TickWave(float DeltaSeconds);
+
+	void StartNextWave();
+	
+	void TickBetweenWave(float DeltaSeconds);
+	
+	virtual void Tick(float DeltaSeconds) override;
+
+	void SpawnRandomMob();
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UClass* CrystalPedestalClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TimeBetweenWaves;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FWaveDefinition> WaveDefinitions;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float TimeBeforeNextWave = 10;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 WaveIndex;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 IsWaveOngoing;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 WaveRemainingMonsters;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 WaveTotalMonsters;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 WaveSpawnedMonsters;
+	
+	UPROPERTY(BlueprintReadOnly)
+	FVector CrystalPosition;
+
+	FWaveDefinition* CurrentWaveDefinition;
+
+	TMap<TSubclassOf<AMonsterAI>, int> RemainingMobToSpawn;
+
+	float SpawnMobInterval;
+	float SpawnMobTimer;
 };
