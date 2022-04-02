af756ca7e9aabd7abcb64577869d9c5e0cca4d40
Thomas <t.foucault2@gmail.com>
2022-04-02 18:35:33 +0200

    Start AI behaviour

---
+++ Source/MedievalTD/MonsterAI.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonsterAI.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+AMonsterAI::AMonsterAI()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SkMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MonsterMesh"));
+	RootComponent = SkMeshComponent;
+}
+
+// Called when the game starts or when spawned
+void AMonsterAI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMonsterAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (m_monsterState == RUN)
+	{
+		SetActorRotation(UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), TargetLocation));
+
+		FVector newPos = FMath::VInterpConstantTo(GetActorLocation(), TargetLocation, DeltaTime, Speed);
+		VelocityMagnitude = (GetActorLocation() - newPos).Size() * Speed;
+	
+		SetActorLocation(newPos, true);
+	}
+
+	if (m_monsterState == ATTACK)
+	{
+		m_attackTimer -= DeltaTime;
+
+		if (m_attackTimer < 0)
+		{
+			m_attackTimer = m_attackInterval;
+
+			Attack();
+		}
+	}
+}
+
+void AMonsterAI::OnBuildingCollisionStart()
+{
+	m_monsterState = ATTACK;
+	m_attackTimer = -1;
+}
+
+void AMonsterAI::OnBuildingCollisionEnd()
+{
+	m_monsterState = RUN;
+}
+
+void AMonsterAI::Attack()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Atacc"));
+}
+
---
+++ Source/MedievalTD/MonsterAI.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MonsterAI.generated.h"
+
+
+
+enum MonsterState
+{
+	RUN,
+	ATTACK
+};
+
+UCLASS()
+class MEDIEVALTD_API AMonsterAI : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMonsterAI();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector TargetLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed = 100.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	float VelocityMagnitude;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* SkMeshComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void OnBuildingCollisionStart();
+	void OnBuildingCollisionEnd();
+
+private:
+	MonsterState m_monsterState = RUN;
+
+	float m_attackInterval;
+	float m_attackTimer;
+
+	void Attack();
+};
